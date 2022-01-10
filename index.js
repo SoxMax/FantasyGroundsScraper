b2d94373a510880811f16c3e3dcb170c38d72dd7
SoxMax <irishsoxmax@gmail.com>
2022-01-10 16:12:23 -0500

    latest

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ export async function scrapeManuever(url) {
     const maneuverDetails = {}
 
     $('div[class=mw-parser-output]', '#content').find('p').each((i, content) => {
-        if($(content).find('b').text().trim()) {
+        if ($(content).find('b').text().trim()) {
             $(content).text().trim().split(';').forEach(field => {
                 const splitField = field.split(':')
-                if(splitField.length > 1) {
+                if (splitField.length > 1) {
                     maneuverDetails[camalize(splitField[0].trim())] = splitField[1].trim()
                 }
             })
@@ -29,25 +29,39 @@ export async function scrapeDiscipline() {
     const baseUrl = 'https://libraryofmetzofitz.fandom.com'
     const html = await axios.get(`${baseUrl}/wiki/Black_Seraph`)
     const $ = await cheerio.load(html.data)
-    const data = []
-  
+    const maneuvers = []
+
+
+    var maneuverPromises = [];
+    $('p').each(function (idx, elem) {
+        var article = new Article({ content: $(this).html() });
+        var promise = article.save();
+        promises.push(promise); // db save async function or other async function
+    });
+    Promise.all(promises)
+        .then(doSomethingOnSuccess)
+        .catch(doSomethingOnError)
+        .finally(mongoose.connection.close);
 
     $('table[class=wikitable]', '#content').each((i, table) => {
-        console.log(i, $(table).find('tr').length)
-        $(table).find('tr').each((i, row) => {
-            if(i > 0 && i < 2) {
+        $(table).find('tr').each(async (i, row) => {
+            if (i > 0 && i < 2) {
                 const firstCell = $(row).find('td').first()
                 const name = $(firstCell).text().trim()
                 const link = `${baseUrl}${$(firstCell).children().attr('href')}`
                 const shortDesc = $(firstCell).next().next().text().trim()
-                const maneuverDetails = await scrapeManuever(link)
-                console.log('details', maneuverDetails)
-                data.push({name, link, shortDesc})
+                const result = Object.assign(await scrapeManuever(link), { name, link, shortDesc })
+                maneuvers.push(result)
             }
         })
         return false
     })
-    console.log(data);
+    return maneuvers
 }
 
-scrapeDiscipline()
+export async function main() {
+    const results = await scrapeDiscipline()
+    console.log(results)
+}
+
+main()
